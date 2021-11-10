04fd05a88f2a09ab7f32983c28f6a6e30673fd36
elRomanoff <83791949+elRomanoff@users.noreply.github.com>
2021-11-09 21:12:22 -0300

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -7,24 +7,36 @@ const Contenedor = require("../Contenedor.js")
 const objProduct = new Contenedor(myOptions,"products")
 
 
-// router.get("/", async (req, res) => {
-//     const arrProducts = await objProduct.getAll()
-//     res.render("index", {arr: arrProducts})
-// })
 
 router.get("/:id", async (req, res) => {
     const id = req.params.id
     const arrProducts = await objProduct.getById(id)
-    res.send({ arr: arrProducts })
+    res.send({ data: arrProducts })
 })
 
 router.post("/", async (req, res) => {
     const product = req.body;
     const idProduct = await objProduct.save(product)
     
-    res.send({"Id":idProduct})
+    res.send({data:idProduct})
+})
+router.delete("/", async (req,res) =>{
+    await objProduct.deleteAll();
+    res.send({data: "All products were successfully deleted"})
 })
 
+router.delete("/:id", async (req,res) =>{
+    const id = req.params.id;
+    const data = await objProduct.deleteById(id);
+    res.send({data: data})
+})
+
+router.put("/:id", async (req,res) =>{
+    const id = req.params.id;
+    const objUpdate = req.body
+    const data = await objProduct.update(id,objUpdate)
+    res.send({data:data})
+})
 
 
 module.exports = router;
